0214d7379a7911f70266ae9a3e088c8b3cd99965
apoorv127 <prasada@uwm.edu>
2020-11-12 12:46:34 -0600

    login issue

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -6,10 +6,15 @@ from rest_framework import viewsets
 # It works by generating a random token string when the user logs in, and then every request we make to that API
 # we add this token string to the request, and that's effectively a password to check every request made is authenticated correctly.
 from rest_framework.authentication import TokenAuthentication
+
+
 from rest_framework import filters
 from profiles_api import serializers
 from profiles_api import models
 from profiles_api import permissions
+
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 class HelloApiView(APIView):
       """Test API View"""
@@ -114,3 +119,9 @@ class UserProfileViewSet(viewsets.ModelViewSet):
       permission_classes = (permissions.UpdateOwnProfile,)
       filter_backends = (filters.SearchFilter, )
       search_fields = ('name', 'email', ) # search by name or email field.
+
+
+class UserLoginApiView(ObtainAuthToken):
+      """Handle creating user authentication tokens"""
+      renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+      
